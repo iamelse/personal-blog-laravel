17cfebca49ea0d03183c7b0f3cffa858bfeffc64
Lana Septiana <lana.septiana2@gmail.com>
2025-03-22 20:43:53 +0700

    fix(tailwind-config): select all files in views folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: [
+    "./resources/views/**/*.blade.php",
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
